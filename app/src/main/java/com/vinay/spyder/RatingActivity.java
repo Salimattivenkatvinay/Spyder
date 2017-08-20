88d60a345508c282d0e95eb44d5f334f5fc2afff
Salimattivenkatvinay <venkatvinay6@gmail.com>
2017-08-20 22:21:52 +0530

    Initial commit

---
+++ app/src/main/java/com/vinay/spyder/RatingActivity.java
@@ -0,0 +1,21 @@
+package com.vinay.spyder;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.root_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
