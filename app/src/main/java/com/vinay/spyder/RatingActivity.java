018bece91512392926c4add574c85950d4f84f45
Salimattivenkatvinay <venkatvinay6@gmail.com>
2017-08-21 12:34:00 +0530

    changes

--- app/src/main/java/com/vinay/spyder/RatingActivity.java
+++ app/src/main/java/com/vinay/spyder/RatingActivity.java
@@ -1,9 +1,11 @@
 package com.vinay.spyder;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RatingActivity extends AppCompatActivity {
 
@@ -17,5 +19,12 @@ public class RatingActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
+
+        findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RatingActivity.this,MainActivity.class));
+            }
+        });
     }
 }
