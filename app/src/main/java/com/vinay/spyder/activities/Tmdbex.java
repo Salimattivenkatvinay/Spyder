340f05e3a458f8e60c95580459131516f58d411a
Vinay Sajjanapu <vinaysajjanapu@gmail.com>
2017-08-26 03:03:50 +0530

    added sample usage of tmdb api

--- app/src/main/java/com/vinay/spyder/activities/Tmdbex.java
+++ app/src/main/java/com/vinay/spyder/activities/Tmdbex.java
@@ -1,10 +1,19 @@
 package com.vinay.spyder.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.vinay.spyder.R;
 
 import info.movito.themoviedbapi.TmdbApi;
@@ -13,29 +22,49 @@ import info.movito.themoviedbapi.model.MovieDb;
 
 public class Tmdbex extends AppCompatActivity {
     MovieDb movie;
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+    TextView result;
+    ImageView imageView;
+    Context context;
+    EditText editText;
+    Button loadData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmdb);
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imgview);
+        editText = findViewById(R.id.edittext);
+        loadData = findViewById(R.id.load);
 
-        MovieTask mt = new MovieTask();
-        mt.execute();
+        context = this;
 
+        loadData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editText.getText().toString().isEmpty()){
+                    new MovieTask(){
+                        protected void onPostExecute(MovieDb movie) {
+                            // Do something with movie
+                            Toast.makeText(getApplicationContext(), movie.toString(), Toast.LENGTH_LONG).show();
+                            result.setText("title: " + movie.getTitle() + "\n" +
+                                    "overview: " + movie.getOverview()+ "\n" +
+                                    "rating: " + movie.getUserRating()+ "\n"
+                            );
+                            Glide.with(context).load("http://image.tmdb.org/t/p/original"+movie.getPosterPath()).into(imageView);
+                        }
+                    }.execute(Integer.parseInt(editText.getText().toString()));
+                }
+            }
+        });
+    }
+    protected class MovieTask extends AsyncTask<Integer, Void, MovieDb> {
+
+        protected MovieDb doInBackground(Integer... params) {
+            TmdbMovies movies = new TmdbApi("7e8f60e325cd06e164799af1e317d7a7").getMovies();
+            return movies.getMovie(params[0],"en");
         }
-
-protected class MovieTask extends AsyncTask<Void, Void, MovieDb> {
-
-    protected MovieDb doInBackground(Void... v) {
-        TmdbMovies movies = new TmdbApi("7e8f60e325cd06e164799af1e317d7a7").getMovies();
-        movie = movies.getMovie(5353, "en");
-        return movie;
     }
-
-    protected void onPostExecute(MovieDb movie) {
-        // Do something with movie
-        Toast.makeText(getApplicationContext(),movie.toString(),Toast.LENGTH_LONG).show();
-    }
-}
 }
 
 
