6dc24f47c81626e98e91bc049fd28acee32d3964
Salimattivenkatvinay <venkatvinay6@gmail.com>
2017-08-27 00:14:40 +0530

    changes

--- app/src/main/java/com/vinay/spyder/activities/LoginActivity.java
+++ app/src/main/java/com/vinay/spyder/activities/LoginActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
--- app/src/main/java/com/vinay/spyder/activities/RatingActivity.java
+++ app/src/main/java/com/vinay/spyder/activities/RatingActivity.java
@@ -1,10 +1,8 @@
 package com.vinay.spyder.activities;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
-import android.support.percent.PercentRelativeLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.vinay.spyder.R;
@@ -15,13 +13,6 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating1);
-
-//        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.root_layout);
-        PercentRelativeLayout percentRelativeLayout= (PercentRelativeLayout) findViewById(R.id.root_layout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) percentRelativeLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2000);
-        animationDrawable.setExitFadeDuration(4000);
-        animationDrawable.start();
 
         findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
             @Override
